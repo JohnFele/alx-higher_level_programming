0c15e7198a2be8637fc4eb3b0e7b61d7b05a6187
JohnFele <fele476@gmail.com>
2023-08-15 15:32:35 +0200

    Fixing

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (97,122):
+for i in range(97, 122):
     if i == 'e' or i == 'q':
         continue
     else:
-        print(f'{i:c}', end='')
+        print('{}'.format(chr(i)), end='')
