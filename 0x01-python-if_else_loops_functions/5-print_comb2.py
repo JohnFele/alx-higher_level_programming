1962761ea44b88fa38fe43a4fb7ad130693b7e8c
JohnFele <fele476@gmail.com>
2023-08-15 15:52:58 +0200

    fixing

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (0,100):
+for i in range(0, 100):
     if i < 99:
-        print(f'{i//10:d}{i%10:d}, ', end='')
+        print('{}{}'.format((i//10), (i % 10)), end='')
     else:
-        print(f'{i//10:d}{i%10:d}')
+        print('{}{}'.format((i//10), (i % 10)))
