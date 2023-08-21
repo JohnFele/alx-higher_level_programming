4a51c1607f59604d396dceeb8b6406d37b562655
JohnFele <fele476@gmail.com>
2023-08-21 16:10:35 +0200

    Projects

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+def add(a, b):
+"""Args:
+    a: first integer
+    b: second integer
+Returns:
+    The return value. a + b
+"""
+    return (a + b)
+
+def sub(a, b):
+"""Args:
+    a: first integer
+    b: second integer
+Returns:
+    The return value. a - b
+"""
+    return (a - b)
+
+def mul(a, b):
+"""Args:
+    a: first integer
+    b: second integer
+Returns:
+    The return value. a * b
+"""
+    return (a * b)
+
+def div(a, b):
+"""Args:
+    a: first integer
+    b: second integer
+Returns:
+    The return value. a / b
+"""
+    return int(a / b)
--- README.md
+++
@@ -1,1 +0,0 @@
-This Directory contains All higher level programming projects
